f84108b5f97b4ec062e722212d0c15acb9bc42ca
Cr-collab <cristiccorrea@gmail.com>
2022-08-13 13:23:14 -0300

    feat : apredendo numeros fracionados;

--- projeto_inical.dart
+++ projeto_inical.dart
@@ -1,9 +1,10 @@
-void main(){
+void main() {
+  print("******** apredendo o objeto inteiro ************");
+  print("******** ************************* *************");
   // int  -  inteiro
-  // double -  numeros fraciondos
 
   int idade = 26;
-  int idade2 =  0x00001A;
+  int idade2 = 0x00001A;
   // criando uma variavel int
   // não pode usar var vazias;
   // valores fracionados não pode ser colocado no tipo int;
@@ -14,5 +15,26 @@ void main(){
   print(idade);
   print(idade2);
 
-  print("Hello, world!");
+  // print("Hello, world!");
+
+  // double -  numeros fraciondos
+  print("******** ************************* *************");
+  print("******** apredendo o objeto double ************");
+  print("******** ************************* *************");
+
+  double altura =  1.81;
+  // NÃO USAMOS VIRGULA PARA NUMEROS FRACIONADOS.
+
+  double exponeciais =  780e6;
+  // forma de reprendentar  780 milhões de form exponecial
+
+  print(exponeciais);
+  print(altura);
+
+    print("******** ************************* *************");
+  print("******** apredendo o objeto double ************");
+  print("******** ************************* *************");
+
+
+
 }
