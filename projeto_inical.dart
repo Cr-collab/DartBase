6a4358e41361b0192952847424bc2622c32edec7
Cr-collab <cristiccorrea@gmail.com>
2022-08-13 13:13:49 -0300

    feat: aprtendo sobre objeto int.

--- projeto_inical.dart
+++ projeto_inical.dart
@@ -1,3 +1,18 @@
 void main(){
+  // int  -  inteiro
+  // double -  numeros fraciondos
+
+  int idade = 26;
+  int idade2 =  0x00001A;
+  // criando uma variavel int
+  // não pode usar var vazias;
+  // valores fracionados não pode ser colocado no tipo int;
+  // o dart aceita valores hexadecimais e qaundo colocamos um
+  // valor ele converte.
+  // 9quadrilhoes
+
+  print(idade);
+  print(idade2);
+
   print("Hello, world!");
 }
