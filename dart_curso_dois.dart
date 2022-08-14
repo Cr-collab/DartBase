ff6a578842792cc2631de1dd6d27d906bc2d21ac
Cr-collab <cristiccorrea@gmail.com>
2022-08-14 19:14:19 -0300

    feat criando uma  classe ;

--- dart_curso_dois.dart
+++ dart_curso_dois.dart
@@ -9,14 +9,23 @@ void main() {
   print(estaMadura);
   mostrarMadura(nome, diasDesdesAColheita, cor: cor, sabor: sabor);
   print(funcQuantosDiasMadura(diasDesdesAColheita));
+
+  Frutas fruta01 = Frutas(
+      cor: cor,
+      sabor: sabor,
+      diasDesdeColheita: diasDesdesAColheita,
+      nome: nome,
+      peso: peso);
+
+
+
 }
 
+int funcQuantosDiasMadura(int dias) {
+  int diasParaMadura = 30;
+  int quantosDiasFaltam = dias - diasParaMadura;
 
-int funcQuantosDiasMadura(int dias){
-   int diasParaMadura = 30;
-   int  quantosDiasFaltam = dias - diasParaMadura;
-
-   return quantosDiasFaltam;
+  return quantosDiasFaltam;
 }
 
 // Posicionais Obrigatorios
@@ -24,14 +33,15 @@ int funcQuantosDiasMadura(int dias){
 // Parametros padrões
 // Modificador  Required
 
-mostrarMadura(String nome, int dias, { String? cor =  "roxo", required String sabor }) {
+mostrarMadura(String nome, int dias,
+    {String? cor = "roxo", required String sabor}) {
   if (dias >= 30) {
     print("A $nome está madura");
   } else {
     print("A $nome não está madura");
   }
 
-  if(cor != null){
+  if (cor != null) {
     print("A $nome é da $cor");
   }
 }
@@ -44,6 +54,19 @@ bool funcEstaMadura(int dias) {
   }
 }
 
+class Frutas {
+  String nome;
+  double peso;
+  String cor;
+  String sabor;
+  int diasDesdeColheita;
+  bool? isMadura;
 
-
-
+  Frutas(
+      {required this.nome,
+      required this.peso,
+      required this.cor,
+      required this.diasDesdeColheita,
+      required this.sabor,
+      this.isMadura});
+}
